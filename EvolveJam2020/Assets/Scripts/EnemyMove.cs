5e530223f8cbbec0f49ddd8a1c42738c8394a4d3
zombykillr123 <jaklein25@gmail.com>
2020-06-26 21:53:40 -0500

    Enemy Basic Move Towards Player

---
+++ EvolveJam2020/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetPlayer;
+
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+    }
+}
