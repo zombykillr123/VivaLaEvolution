1ce99de32c5d9d0b10637efb9ae3fe24edfadafc
zombykillr123 <jaklein25@gmail.com>
2020-06-27 11:50:52 -0500

    scripts and timer sort of

--- EvolveJam2020/Assets/Scripts/Pickup.cs
+++ EvolveJam2020/Assets/Scripts/Pickup.cs
@@ -8,10 +8,20 @@ public class Pickup : MonoBehaviour
     [SerializeField]
     private int myType; // not using enums right now, can do it later
 
+    [SerializeField]
+    private float timerDefault;
+
+    private float currentTime;
+
+    private GameObject timerObject;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.pickupColors[myType];
+
+        timerObject = transform.GetChild(0).gameObject;
+        timerObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,13 +30,42 @@ public class Pickup : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Player":
+                // Start counting down
+                currentTime = timerDefault;
+                timerObject.SetActive(true);
+                StartCoroutine(TimerToPickup());
+                break;
+            case "Enemy":
+                GameManager.instance.LosePickup(myType, true);
+                Destroy(gameObject);
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Hit");
-            GameManager.instance.StorePickups(myType);
-            Destroy(gameObject);
+            // Resest timer stats
+            StopCoroutine(TimerToPickup());
+            timerObject.SetActive(false);
         }
+    }
+
+    IEnumerator TimerToPickup()
+    {
+        while (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            yield return null;
+        }
+        // When timer is 0, destroy it
+        GameManager.instance.LosePickup(myType, false);
+        Destroy(gameObject);
     }
 }
